f71f1875a19c5b14dd9ba323548be650b2a2d15b
Piyush <Piyusshh@users.noreply.github.com>
2020-09-27 13:32:44 +0530

    mail using smtp server

--- mail.php
+++ mail.php
@@ -1,14 +1,28 @@
 <?php
 
-$to = "piyushagrawal010@gmail.com";
-$subject = "Response From Website";
-$message = "I am Very Thankful to you";
-$header = "From: piyushagrawal010@gmail.com";
+use PHPMailer\PHPMailer\PHPMailer; 
+use PHPMailer\PHPMailer\Exception;
 
-if(mail($to, $subject, $message, $header)){
-    echo "Mail Send successfully";
-}else{
-    echo "Cannot send mail";
+require 'vendor/autoload.php'; 
+
+$mail = new PHPMailer(); // create a new object
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 465; // or 587
+$mail->IsHTML(true);
+$mail->Username = "piyushagrawal010@gmail.com";
+$mail->Password = "Agrawal@1998";
+$mail->SetFrom("piyushagrawal@gmail.com");
+$mail->Subject = "Test";
+$mail->Body = "hello";
+$mail->AddAddress("piyushagrawal010@gmail.com");
+if(!$mail->Send()) {
+echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+echo "Message has been sent";
 }
 
 ?>
